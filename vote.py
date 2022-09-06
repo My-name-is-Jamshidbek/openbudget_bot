4eabc35ab549c314a486163465ee5978156f907c
Professional <ZiyodullaDeveloper@gmail.com>
2022-09-06 15:12:47 +0500

    hammasi tayyor

--- vote.py
+++ vote.py
@@ -5,22 +5,25 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def vote(phone_number, kod, token):
-    phone_number = '998'+str(phone_number)
+    phone_number = '998' + str(phone_number)
     try:
-    # phone_number = "998992231638"
-    # kod = "546546"
-    # token = "TFVFBK46GJT5EJU3IILDZXWRZLCSLRQA"
+        # phone_number = "998992231638"
+        # kod = "546546"
+        # token = "TFVFBK46GJT5EJU3IILDZXWRZLCSLRQA"
         votes = requests.post('https://admin.openbudget.uz/api/v1/user/temp/vote/',
                               data={'phone': phone_number, 'otp': kod, 'token': token,
                                     'application': "123288"}, verify=False)
-    except: return False
-    try:a = votes.json()
+    except:
+        return False
+    try:
+        a = votes.json()
     except:
         return 'rt'
     try:
         a = a.get('detail')
         return a
-    except:return False
+    except:
+        return False
 # except:
 #     return False
 
